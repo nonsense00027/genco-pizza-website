087a50a3f1294cf2e534c2df901b7051ad192190
artpersona <jp11perez@gmail.com>
2020-10-25 08:59:04 +0800

    fixing

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,21 +1,21 @@
 const action = {
-  ADD_TO_POSTS: "ADD_TO_POSTS",
-};
-
-// export const getNumberOfPosts = (post) => post?.reduce((total, item) => (total+item.price),0)
-
-const reducer = (state, action) => {
-  console.log("action is:", action);
-  switch (action.type) {
-    case action.ADD_TO_POSTS:
-      return {
-        ...state,
-        posts: [...state.posts, action.post],
-      };
-
-    default:
-      return state;
-  }
-};
-
-export default reducer;
+    ADD_TO_POSTS: "ADD_TO_POSTS",
+  };
+  
+  // export const getNumberOfPosts = (post) => post?.reduce((total, item) => (total+item.price),0)
+  
+  const reducer = (state, action) => {
+    console.log("action is:", action);
+    switch (action.type) {
+      case action.ADD_TO_POSTS:
+        return {
+          ...state,
+          posts: [...state.posts, action.post],
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
