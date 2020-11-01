a91162d3ae7967a758f5edd34120ae568bf43603
nonsense00027 <caneteneilfred@gmail.com>
2020-11-01 10:54:35 +0800

    Initial Home Page design update

--- src/components/Home/Pizza.js
+++ src/components/Home/Pizza.js
@@ -1,36 +1,58 @@
 import React, { useState } from "react";
 import "./Pizza.css";
 import pizza from "../../img/pizza.jpg";
-import { Button, Inpu, IconButton } from "@material-ui/core";
+import { Button, Inpu, IconButton, Badge } from "@material-ui/core";
 import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 function Pizza({ name, price, category, id }) {
   const [qty, setQty] = useState(1);
+
+  const increment = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decrement = () => {
+    if (qty > 1) {
+      setQty((prevQty) => prevQty - 1);
+    }
+  };
+
   return (
     <div className="pizza">
       <div className="pizza__header">
         <img src={pizza} alt="" />
+        <div className="pizza__price">
+          <p>P {price}.00</p>
+        </div>
       </div>
       <div className="pizza__body">
         <h2 className="pizza__name">{name}</h2>
+        <div className="pizza__sizes">
+          <Button className="pizza__size selected">12"</Button>
+          <Button className="pizza__size">14"</Button>
+          <Button className="pizza__size">16"</Button>
+        </div>
       </div>
       <div className="pizza__footer">
-        {/* <input type="number" name="" id="" /> */}
-        <form>
-          <div className="pizza__qty">
-            <h2>{qty}</h2>
-            <div className="pizza__qtyOptions">
-              <IconButton className="pizza__qtyButton">
-                <ArrowDropUpIcon />
-              </IconButton>
-              <IconButton className="pizza__qtyButton">
-                <ArrowDropDownIcon />
-              </IconButton>
-            </div>
+        <div className="pizza__qty">
+          <input type="text" value={qty} disabled />
+          <div className="pizza__qtyOptions">
+            <IconButton className="pizza__qtyButton">
+              <ArrowDropUpIcon
+                className="pizza__qtyButtonOption"
+                onClick={increment}
+              />
+            </IconButton>
+            <IconButton className="pizza__qtyButton">
+              <ArrowDropDownIcon
+                className="pizza__qtyButtonOption"
+                onClick={decrement}
+              />
+            </IconButton>
           </div>
-          <Button>add to order</Button>
-        </form>
+        </div>
+        <Button className="pizza__order">add order</Button>
       </div>
     </div>
   );
--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -3,7 +3,9 @@ import "./Products.css";
 import { useParams } from "react-router-dom";
 import { useStateValue } from "../../DataLayer";
 import Pizza from "./Pizza";
-
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Button } from "@material-ui/core";
 function Products() {
   const { category } = useParams();
   const [{ products, displayProducts }, dispatch] = useStateValue();
@@ -13,20 +15,40 @@ function Products() {
       type: "GET_DISPLAY_PRODUCTS",
       payload: category,
     });
-  }, [category, displayProducts]);
+  }, [category, products]);
 
   console.log("category", category);
   console.log("display products", displayProducts);
 
   return (
     <div className="products">
+      <div className="products__header">
+        <div className="products__search">
+          <input type="text" />
+          <SearchIcon />
+        </div>
+        <div className="products__options">
+          <Button className="products__login">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"
+              alt=""
+              className="products__loginLogo"
+            />
+            Login
+          </Button>
+          <Button className="products__cart">
+            <ShoppingBasketIcon className="products__cartLogo" />
+            Orders
+          </Button>
+        </div>
+      </div>
       <div className="products__body">
         {displayProducts.map((product) => (
           <Pizza
             name={product.name}
             id={product.id}
             category={product.category}
-            price={product.category}
+            price={product.price}
           />
         ))}
       </div>
